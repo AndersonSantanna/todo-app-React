e5fbd9ba5f8f35b5fcca68304a2cbc5d0858561f
AndersonSantanna <anderson.santanna14@gmail.com>
2018-12-06 02:25:34 -0200

    form and buttons

---
+++ frontend/src/todo/todoForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Grid from '../template/grid'
+import IconButton from '../template/iconButton'
+
+export default props =>(
+    <div role='form' className='todoFrom'>
+        <Grid cols='10 10 11 11'>
+            <input type="text" className='form-control' placeholder='Adicione uma tarefa'/>
+        </Grid>
+        <Grid cols='1 1 1 1'>
+            <IconButton style='primary' icon='plus'></IconButton>
+        </Grid>
+    </div>
+)
