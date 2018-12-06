b75b346c5e859e461a8baaaefc6691a1dfcbb500
AndersonSantanna <anderson.santanna14@gmail.com>
2018-12-06 16:13:21 -0200

    adicionando, removendo e excluindo

--- frontend/src/template/iconButton.jsx
+++ frontend/src/template/iconButton.jsx
@@ -1,25 +1,11 @@
 import React from 'react'
+import If from './if'
 
-export default props =>{
-    if(props.hide) {
-        return  null 
-    } else{
-        return ( 
+export default props =>(
+    <If name={!props.hide}>
         <button className={'btn btn-' + props.style} onClick={props.onClick}>
             <i className={'fa fa-'+props.icon}></i>
-        </button>)
-    }
-}
-/**Não funcionou a tag If, logica abaixo
- * 
- *  import React from 'react'
-    import If from './if'
-
-    export default props =>(
-        <If test={!props.hide}>
-            <button className={'btn btn-' + props.style} onClick={props.onClick}>
-                <i className={'fa fa-'+props.icon}></i>
-            </button>)
-        </If>
-    )
- */
+        </button>
+    </If>
+)
+ 
--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -5,10 +5,14 @@ import IconButton from '../template/iconButton'
 export default props =>(
     <div role='form' className='todoFrom'>
         <Grid cols='10 10 11 11'>
-            <input type="text" className='form-control' placeholder='Adicione uma tarefa'/>
+            <input type="text" 
+            className='form-control' 
+            placeholder='Adicione uma tarefa' 
+            value={props.description}
+            onChange={props.handleChange}/>
         </Grid>
         <Grid cols='1 1 1 1'>
-            <IconButton style='primary' icon='plus' onCick={props.handleAdd}></IconButton>
+            <IconButton style='primary' icon='plus' onClick={props.handleAdd}></IconButton>
         </Grid>
     </div>
 )
