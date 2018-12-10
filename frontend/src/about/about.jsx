6388543625057ac7fdc7363d04854a2036dbba34
AndersonSantanna <anderson.santanna14@gmail.com>
2018-12-10 15:36:52 -0200

    correção de tabulação

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -13,17 +13,17 @@ export default props => (
         </p>
         <h2>Missão e Visão</h2>
         <p>
-        Venenatis vitae augue urna habitasse lacinia euismod
-        nunc fermentum neque id neque felis, fames per molestie
-        ut iaculis donec nibh vitae etiam hac aliquet.
+            Venenatis vitae augue urna habitasse lacinia euismod
+            nunc fermentum neque id neque felis, fames per molestie
+            ut iaculis donec nibh vitae etiam hac aliquet.
         </p>
         <h2>Informações</h2>
         <p>
-        Aptent id curabitur orci ornare sapien volutpat massa 
-        dictum habitasse convallis, odio tristique libero fermentum 
-        dictumst tellus ultrices senectus platea tortor ut, 
-        placerat enim ullamcorper odio neque consectetur aenean 
-        libero massa.
+            Aptent id curabitur orci ornare sapien volutpat massa 
+            dictum habitasse convallis, odio tristique libero fermentum 
+            dictumst tellus ultrices senectus platea tortor ut, 
+            placerat enim ullamcorper odio neque consectetur aenean 
+            libero massa.
         </p>
     </div>
 )
